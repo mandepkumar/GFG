4ef633a560b9f70194094405884c716488de32bb
mk <mandeepk7856@gmail.com>
2025-03-02 23:05:54 +0530

    my string class

---
+++ src/Experiment/MyString.java
@@ -0,0 +1,16 @@
+package Experiment;
+
+
+class string{
+    string(String str){
+
+    }
+}
+public class MyString {
+    public static void main(String[] args) {
+
+        string str = new string("Str");
+        System.out.println(str);
+    }
+}
+
